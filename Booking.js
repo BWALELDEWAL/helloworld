2d40753c8ea75fc07942c9889b6846a502c8eecb
yehia249 <yehiawael@icloud.com>
2025-03-07 09:33:52 +0200

    part of booking

--- Booking.js
+++ Booking.js
@@ -1,1 +1,13 @@
- 
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+    user: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true 
+    },
+    event: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Event', 
+        required: true 
+    },
